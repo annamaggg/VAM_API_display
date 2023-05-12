9621b6ac81d43b4993c3f43df0c2fe2b663d4e50
annamaggg <annamagnusson99@gmail.com>
2023-05-12 13:34:19 +0100

    added search routes to return data by location

--- app.rb
+++ app.rb
@@ -9,23 +9,72 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  get '/' do
-    "Hello World"
-    @glass_items = []
-    response = HTTParty.get("https://api.vam.ac.uk/v2/objects/search?id_material=AAT10797")
+  def location_req(param)
+    @item_list = []
+    response = HTTParty.get("https://api.vam.ac.uk/v2/objects/search?#{param}")
     if response.code == 200
       data = response.parsed_response['records']
       data.each do |item|
-        @glass_items << {
-          'name' => item['objectType'],
+        @item_list << {
+          'name' => item['_primaryTitle'],
+          'type' => item['objectType'],
           'date' => item['_primaryDate'],
           'imageID' => item['_primaryImageId']
         }
       end
-      puts @glass_items
     else 
       puts "bad req"
     end
-      return erb(:index)
+    return erb(:index)
+  end
+
+  get '/' do
+    @item_list = []
+    return erb(:index)
+  end
+
+  get '/search/London' do
+    location_req('q_place_name=London')
+  end
+
+  get '/search/Amsterdam' do
+    location_req('id_place=x28722')
   end
 end
+#   get '/' do
+#     "Hello World"
+#     @glass_items = []
+#     response = HTTParty.get("https://api.vam.ac.uk/v2/objects/search?id_material=AAT10797")
+#     if response.code == 200
+#       data = response.parsed_response['records']
+#       data.each do |item|
+#         @glass_items << {
+#           'name' => item['objectType'],
+#           'date' => item['_primaryDate'],
+#           'imageID' => item['_primaryImageId']
+#         }
+#       end
+#       # puts @glass_items
+#     else 
+#       puts "bad req"
+#     end
+
+#     @london_items = []
+#     response = HTTParty.get("https://api.vam.ac.uk/v2/objects/search?q_place_name=London")
+#     if response.code == 200
+#       data = response.parsed_response['records']
+#       data.each do |item|
+#         @london_items << {
+#           'type' => item['objectType'],
+#           'name' => item['_primaryTitle'],
+#           'date' => item['_primaryDate'],
+#           'imageID' => item['_primaryImageId']
+#         }
+#       end
+       
+#     else 
+#       puts "bad req"
+#     end
+#       return erb(:index)
+#   end
+ 
