928b91b2602fa8f18d51eddbdc5f1bd940aac838
annamaggg <annamagnusson99@gmail.com>
2023-05-15 12:39:05 +0100

    p5.js library and wave model added to project for image manipulation

--- app.rb
+++ app.rb
@@ -9,6 +9,8 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  set :public_folder, File.dirname(__FILE__) + '/public'
+
   def location_req(param)
     @item_list = []
     response = HTTParty.get("https://api.vam.ac.uk/v2/objects/search?#{param}")
@@ -66,41 +68,10 @@ class Application < Sinatra::Base
   get '/search/Tokyo' do
     location_req('id_place=x32204')
   end
-end
-#   get '/' do
-#     "Hello World"
-#     @glass_items = []
-#     response = HTTParty.get("https://api.vam.ac.uk/v2/objects/search?id_material=AAT10797")
-#     if response.code == 200
-#       data = response.parsed_response['records']
-#       data.each do |item|
-#         @glass_items << {
-#           'name' => item['objectType'],
-#           'date' => item['_primaryDate'],
-#           'imageID' => item['_primaryImageId']
-#         }
-#       end
-#       # puts @glass_items
-#     else 
-#       puts "bad req"
-#     end
 
-#     @london_items = []
-#     response = HTTParty.get("https://api.vam.ac.uk/v2/objects/search?q_place_name=London")
-#     if response.code == 200
-#       data = response.parsed_response['records']
-#       data.each do |item|
-#         @london_items << {
-#           'type' => item['objectType'],
-#           'name' => item['_primaryTitle'],
-#           'date' => item['_primaryDate'],
-#           'imageID' => item['_primaryImageId']
-#         }
-#       end
-       
-#     else 
-#       puts "bad req"
-#     end
-#       return erb(:index)
-#   end
+  get '/style-image' do 
+    return erb(:style_transfer)
+  end
+end
+
  
---
+++ public/p5.js
@@ -0,0 +1,1 @@
+https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js
