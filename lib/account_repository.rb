acec82f4ebd0ce9fefb30408664c75ca44addfc9
annamaggg <annamagnusson99@gmail.com>
2023-05-25 16:00:54 +0100

    create and read functions created and tested in account repository

---
+++ lib/account_repository.rb
@@ -0,0 +1,26 @@
+require_relative 'account'
+require_relative 'database_connection'
+
+class AccountRepository
+  def all
+    sql = 'SELECT id, username, email, passkey FROM accounts;'
+      results = DatabaseConnection.exec_params(sql, [])
+      
+      accounts = []
+      results.each do |item|
+        account = Account.new
+        account.id = item['id']
+        account.username = item['username']
+        account.email = item['email']
+        account.passkey = item['passkey']
+        accounts << account
+      end
+      return accounts
+  end
+
+  def create(account)
+    sql = 'INSERT INTO accounts (username, email, passkey) VALUES ($1, $2, $3)'
+    result = DatabaseConnection.exec_params(sql, [account.username, account.email, account.passkey])
+    return result
+  end
+end
---
+++ spec/account_repository_spec.rb
@@ -0,0 +1,33 @@
+require 'account_repository'
+require 'database_connection'
+
+def reset_tables 
+  seed_sql = File.read('seeds/tables_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'my_va_test' })
+  connection.exec(seed_sql)
+end
+
+describe AccountRepository do 
+  before(:each) do 
+    reset_tables
+  end
+
+    it "returns all accounts" do
+      repo = AccountRepository.new
+      expect(repo.all.length).to eq(2)
+      expect(repo.all.last.username).to eq('GB')
+    end
+
+    it "creates an account" do 
+      repo = AccountRepository.new
+      account = Account.new
+      account.username = "tommag"
+      account.email = "tommag@email.com"
+      account.passkey = "pass3"
+      repo.create(account)
+
+      expect(repo.all.length).to eq(3)
+      expect(repo.all.last.username).to eq('tommag')
+      expect(repo.all.last.passkey).to eq('pass3')
+  end
+end
