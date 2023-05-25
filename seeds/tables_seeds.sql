92ab186b1235293aeb9b041dbe5b0c22af7fe914
annamaggg <annamagnusson99@gmail.com>
2023-05-25 15:44:06 +0100

    seed data added for test database

--- seeds/tables_seeds.sql
+++ seeds/tables_seeds.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS accounts, artefacts;
 
 CREATE TABLE accounts (
   id SERIAL PRIMARY KEY,
@@ -12,9 +13,17 @@ CREATE TABLE artefacts (
   title text,
   time_period text,
   object_type text,
-  image_id int,
+  image_id text,
   account_id int,
   constraint fk_account foreign key(account_id)
     references accounts(id)
     on delete cascade
 );
+
+TRUNCATE TABLE accounts, artefacts RESTART IDENTITY;
+
+INSERT INTO accounts (username, email, passkey) VALUES ('AM', 'annamag@email.com', 'pass1');
+INSERT INTO accounts (username, email, passkey) VALUES ('GB', 'gerry@email.co.uk', 'pass2');
+
+INSERT INTO artefacts (title, time_period, object_type, image_id, account_id) VALUES ('Lilies', '1860', 'Painting', '123', (SELECT id FROM accounts WHERE username='AM'));
+INSERT INTO artefacts (title, time_period, object_type, image_id, account_id) VALUES ('Swan', '1990', 'Glass sculpture', '456', (SELECT id FROM accounts WHERE username='AM'));
